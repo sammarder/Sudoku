a359d1c98996f406c636361f90e353dd7688c37a
Sam Marder <smardercollege@gmail.com>
2015-03-14 21:25:35 -0400

    moving files

--- Sudoku/src/main/IController.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowListener;
-
-public interface IController extends ActionListener, WindowListener{
-	public void actionPerformed(ActionEvent event);
-}
--- Sudoku/src/main/ISubject.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public interface ISubject {
-	void addObserver(IObserver observer);
-	void removerObserver(IObserver observer);
-	void notifyObservers();
-}
